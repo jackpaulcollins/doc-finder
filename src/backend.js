bce0fa88bee0590c2894391690020d94dac4185b
Jack Collins <jackcollins@Jacks-MBP.lan>
2019-03-22 11:23:23 -0700

    expand backend to return doctors in oregon, their phone number, and address

--- src/backend.js
+++ src/backend.js
@@ -5,8 +5,8 @@ export class ApiCall{
   newDataCall(){
     return new Promise(function(resolve,reject){
     let request = new XMLHttpRequest();
-    const location = 'location=37.773%2C-122.413%2C100&user_location=45.5122%2C122.6587';
-    let url = 'https://api.betterdoctor.com/2016-03-01/doctors?' + location + '&skip=0&limit=10&user_key=' + process.env.exports.apiKey
+    const location = 'OR';
+    let url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=' + location + '&skip=0&limit=10&user_key=' + process.env.exports.apiKey
     request.onload = function(){
       if (this.status == 200){
         resolve(request.response);
@@ -18,4 +18,22 @@ export class ApiCall{
       request.send();
     });
   }
+}
+
+export function parseData(input){
+  const data = input.data;
+  const output = [];
+  data.forEach(function(obj){
+    output.push(obj.practices[0].name,obj.practices[0].phones[0].number,obj.practices[0].visit_address.street,obj.practices[0].visit_address.city,obj.practices[0].visit_address.state)
+  })
+  console.log(output)
+  return output
+}
+
+export function parseString(string){
+  let output = [];
+  for (let i=0;i<string.length;i++){
+    output.push(string[i] + '<br>');
+  }
+  return output
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import './styles.scss';
 import $ from 'jquery';
 import './backend.js';
 import {ApiCall} from './backend.js'
+import { parseData } from './backend.js'
+import { parseString } from './backend.js'
 
 $(document).ready(function(){
   $("#submit").submit(function(){
@@ -12,7 +14,9 @@ $(document).ready(function(){
     promise.then(function(response){
       let body = JSON.parse(response);
       console.log(body);
-      $(".output-field").text(body);
+      const output = parseData(body);
+      const display = parseString(output);
+      $(".output-field").html(display);
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
     });
